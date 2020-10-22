a66e9ac14600f662c9de6fd20f613c694e251e28
amiraminuddin <amiraminuddin@gmail.com>
2020-10-22 12:45:44 +0800

    Initial Commit

--- script.py
+++ script.py
@@ -4,12 +4,6 @@ from os import rename
 
 import requests
 
-print(sys.version)
-print(sys.executable)
-
-def greet(who_to_greet):
-	greeting = 'Hello, {}'.format(who_to_greet)
-	return greeting
-
 r = requests.get('https://www.youtube.com/watch?v=RMH6X5VKEuY&list=RDRMH6X5VKEuY&start_radio=1')
 print(r.status_code)
+print(r.ok)
